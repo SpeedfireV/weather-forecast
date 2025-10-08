6e3dd6e91a38b96f4b2ffea9ea93ff6aa8f295c0
SpeedfireV <grzegorz.niespodzianek@gmail.com>
2025-10-08 23:46:16 +0200

    Limited access to modyfing forecast data

--- Mini Weather Journal NET/Mini Weather Journal/Controllers/WeatherController.cs
+++ Mini Weather Journal NET/Mini Weather Journal/Controllers/WeatherController.cs
@@ -48,6 +48,7 @@ public class WeatherController: ControllerBase
     }
 
     [HttpPut]
+    [Authorize(Roles = "Admin")]
     public async Task<IActionResult> UpdateWeatherForecast([FromQuery] WeatherForecast weatherForecast)
     {
         try
@@ -65,6 +66,7 @@ public class WeatherController: ControllerBase
     
 
     [HttpPost]
+    [Authorize(Roles = "Admin")]
     public async Task<IActionResult> AddWeatherForecast(AddWeatherForecastDto dto)
     {
         var weatherForecast = new WeatherForecast
@@ -124,7 +126,7 @@ public class WeatherController: ControllerBase
     }
     
     [HttpGet("forecast")]
-    public IActionResult GetWeatherForecast([FromQuery] int days)
+    public IActionResult GetWeatherForecast([FromBody] int days)
     {
         try
         {
@@ -140,7 +142,7 @@ public class WeatherController: ControllerBase
     }
     
     [HttpGet("date")]
-    public IActionResult GetWeatherDate([FromQuery] DateOnly date)
+    public IActionResult GetWeatherDate([FromBody] DateOnly date)
     {
         try
         {
